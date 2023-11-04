2c56fe3da51adb311c94de1743b716a7910a6383
Bruno Alla <browniebroke@users.noreply.github.com>
2023-11-04 17:04:32 +0000

    feat: add the ability to run with python -m deezer_oauth (#394)

---
+++ tests/test_dunder_main.py
@@ -0,0 +1,12 @@
+import subprocess
+import sys
+
+
+def test_can_run_as_python_module():
+    result = subprocess.run(
+        [sys.executable, "-m", "deezer_oauth", "--help"],  # noqa S603,S607
+        check=True,
+        capture_output=True,
+    )
+    assert result.returncode == 0
+    assert b"deezer-oauth [OPTIONS]" in result.stdout
